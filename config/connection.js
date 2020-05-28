23651ad471b3e662be7b257565a46f4e7ca62acc
Joseph Dvorak <dvorakjt@gmail.com>
2020-05-27 23:45:18 -0400

    added jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 connection.connect((err) => {
     if (err) throw err;
